0f258249bb0977c18550e4755d5253954a12e9e3
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-03-29 14:48:12 +0400

    "moved script to external file for clarity" "Remove() used to allow for game restart, game goes off-course on second round, but plays"

--- script.js
+++ script.js
@@ -1,108 +1,136 @@
 
-    // The algorithm of the game: 
+   // variable for computer calculation for the function
+   let computerCalculation;
 
-// Create a variable for computer calculation 
-let computerCalculation;
+   // define computer choice globally
+   let computerGeneratedChoice;
 
+   //define user input globally clear
+   let playerGeneratedChoice;
 
-// computerChoice is created globally
-let computerChoice;
+   // track the wins
+   let computerVictories = 0;
+   let playerVictories = 0;
 
-// User input is defined globally, but has to be changeable (so the value is defined only within the loop)
-let playerChoice;
+   // defining DOM elements to modify in the functions 
+   const displayContainer = document.querySelector(".displayContainer");
+   const roundResultDisplay = document.querySelector(".roundResultDisplay");
+   const choiceContainer = document.querySelector(".container");
+       choiceContainer.addEventListener("click", winner);
 
-//Track the wins
-let computerVictories = 0;
-let playerVictories = 0;
+   // Result display 
+   const playerScore = document.querySelector(".playerScore");
+   const computerScore = document.querySelector(".computerScore");
+   const pointsToWin = document.querySelector(".pointsToWin");
 
+   // Post Result Display
+   const afterLoss = document.querySelector(".afterLoss");
+   const afterWin = document.querySelector(".afterWin");
+   const playAgain = document.querySelector(".playAgain");
 
-function winner(player, computer) {
-// The computer randomly generates a number from 0 to 99
-computerCalculation = Math.floor(Math.random() * 100);
-
-// That number is then divided by 33; 0 to 33 is scissors, <33 to 66 is paper, and <66 to 99 is rock
- if (computerCalculation <= 33) {
-    computerChoice = "scissors";
-} else if (computerCalculation >33 && computerCalculation <=66){    
-    computerChoice = "paper"; 
-} else {
-    computerChoice = "rock";
-}
-
-// the player is asked for the choice
-// Player inputs 1 of the 3 options: Rock Paper or Scissors
-    let playerInput = prompt("Choose Rock | Paper | Scissors", " ");
-// The user choice is case-insensitive so the input is converted to lower case for consistency
-playerChoice = playerInput.toLowerCase();
-
- // We then evaluate the computer choice against the user choice, and declare a winner of the round
-    switch (playerChoice) {
-        //case part 1: user chose scissors
-        case "scissors":
-            if (computerChoice === "rock") {
-                ++computerVictories;
-                console.log("You lost this round, the computer chose " + computerChoice);
-                console.log("Your score is " + playerVictories + " while computer score is " + computerVictories);
-            } else if (computerChoice === "paper") {
-                ++playerVictories;
-                console.log("You won this round!");
-                console.log("The computer's choice was " + computerChoice);
-                console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-            } else {
-                console.log("It's a draw!");
-                console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-            }
-        break;
-
-            //case part 2: user chose paper
-        case "paper":
-            if (computerChoice === "rock") {
-                ++playerVictories;
-                console.log("You won this round!");
-                console.log("The computer's choice was " + computerChoice);
-                console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-            } else if (computerChoice === "paper") {
-                console.log("It's a draw!");
-                console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-            } else {
-                ++computerVictories;
-                console.log("You lost this round, the computer chose " + computerChoice);
-                console.log("Your score is " + playerVictories + " while computer score is " + computerVictories); 
-            }
-        break;
-            //case part 3: user chose rock
-            case "rock":
-                if (computerChoice === "rock") {
-                    console.log("It's a draw!");
-                    console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-                } else if (computerChoice === "paper") {
-                    ++computerVictories;
-                    console.log("You lost this round, the computer chose " + computerChoice);
-                    console.log("Your score is " + playerVictories + " while computer score is " + computerVictories); 
-                } else {
-                    ++playerVictories;
-                    console.log("You won this round!");
-                    console.log("The computer's choice was " + computerChoice);
-                    console.log("Your score is " + playerVictories + " while the computer score is " + computerVictories);
-                }
-            break;
-    }
-}
+    function winner(e) {
+       playerChoice = e.target.id;
+       computerChoiceGenerator();
+       // evaluate comp choice vs user choice 
+       if (playerChoice === computerChoice) {
+           roundResultDisplay.textContent = "You chose " + playerChoice + " while the computer also chose " + computerChoice + " it's a draw this round!"
+           //add a score display
+           displayContainer.appendChild(roundResultDisplay);
+       }
+       switch (playerChoice) {
+           case "scissors":
+               if (computerChoice === "rock") {
+                   ++computerVictories;
+                   victoryDisplay();
+                   revealChoices();
+               } else if (computerChoice === "paper") {
+                   ++playerVictories;
+                   victoryDisplay();
+                   revealChoices();
+               } 
+               break;
+           
+           //part 2: user chose paper
+           case "paper":
+               if (computerChoice === "rock") {
+                   ++playerVictories;
+                   victoryDisplay();
+                   revealChoices();
+               } else if (computerChoice === "scissors") {
+                   ++computerVictories;
+                   victoryDisplay();
+                   revealChoices();
+               }
+               break;
+           
+           //part 3: user chose rock
+           case "rock":
+               if (computerChoice === "paper") {
+                   ++computerVictories
+                   victoryDisplay();
+                   revealChoices();
+               } else if (computerChoice === "scissors") {
+                   ++playerVictories;
+                   revealChoices();
+               }
+       }
+       scoreCounter();
+   }
 
 
+   function scoreCounter() {
+       //if a player or computer wins, a victory message is displayed
+       if (computerVictories === 5) {
+           clearGame();
+           afterLoss.textContent = "You lost this game by " + (computerVictories - playerVictories) + " points. Don't worry, you can try again."
+           playAgain.textContent = "Play Again?"
+       } else if (playerVictories === 5) {
+           clearGame();
+           afterWin.textContent = "YOU WON! You were up by " + (playerVictories - computerVictories) + " points. Well done!"
+           playAgain.textContent = "Play Again?"
+       } 
+   } 
 
-// The looped game must keep track of victories, and stop whenever 1 of the 2 players wins 5 times in a single game
-while(computerVictories < 5 || playerVictories < 5) {
-// if a player or computer wins, a victory message must be displayed
-if (computerVictories === 5) {
-    console.log("The computer won, you lost by " + (computerVictories - playerVictories) + " point(s)");
-    break;
-} else if (playerVictories === 5) {
-    console.log("Congratulations!!! You won!!!! wooo")
-    console.log("You were up by " + (playerVictories - computerVictories) + " point(s)! Well Done")
-    break;
-} else {
-    winner(playerChoice, computerChoice);
-}
-}
+
+   function computerChoiceGenerator() {
+       // Random number between 0 and 99 
+       computerCalculation = Math.floor(Math.random() * 100);
+
+       //divide number into 3 parts, to feign a computer choice
+       if (computerCalculation <= 33) {
+       computerChoice = "scissors";
+       } else if (computerCalculation >33 && computerCalculation <=66){    
+       computerChoice = "paper"; 
+       } else {
+       computerChoice = "rock";
+       }
+   }
+   
+   function victoryDisplay() {
+       computerScore.textContent = "The computer has " + computerVictories + " points.";
+       playerScore.textContent = "You have " + playerVictories + " points.";
+       pointsToWin.textContent = "You need just " + (5 - playerVictories) + " more points to win!";
+   }
+
+   function revealChoices() {
+       roundResultDisplay.textContent = "You chose " +playerChoice + " while the computer chose " + computerChoice + ".";
+   }
+
+   //upon gameOver, clear games for another match
+   function clearGame() {
+       playerScore.textContent = '';
+       computerScore.textContent = '';
+       pointsToWin.textContent = '';
+   }
+   
+   //play again reset function 
+   playAgain.addEventListener("click", restartGame);
+
+   function restartGame() { 
+       playerVictories = 0;
+       computerVictories = 0;
+       afterLoss.remove();
+       afterWin.remove();
+       playAgain.remove();
+   }
 
